7b9939471d9350537f3af7e92b7fbbe7d798bb88
erikcrossdev <erikcruzdev@gmail.com>
2022-09-03 15:36:42 -0300

    Add smooth rotation on stick

--- Mouse2022/Assets/MouseFollow.cs
+++ Mouse2022/Assets/MouseFollow.cs
@@ -5,12 +5,18 @@ using DG.Tweening;
 
 public class MouseFollow : MonoBehaviour
 {
-    public AnimationCurve curve;
     public float Distance = 10;
     public GameObject staff;
+    public GameObject staffPivot;
     public Vector3 punch;
     private Vector3 originalScale;
     public Animator anim;
+    Vector3 pos1;
+    Vector3 pos2;
+
+    public float velocity1;
+    public float velocity2;
+    public float velocity3;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +28,20 @@ public class MouseFollow : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Vector3 pos = ray.GetPoint(Distance);
-        transform.position = Vector3.Lerp(transform.position, pos, curve.Evaluate(0.01f));
+        
+        var target = pos;
+
+        pos1 = Vector3.Lerp(pos1, target, Time.deltaTime * velocity1);
+        pos2 = Vector3.Lerp(pos2, pos1, Time.deltaTime * velocity2);
+
+        pos2.y = pos1.y - velocity3;
+
+        var vel = pos2 - pos1;
+
+        var angle = Mathf.Atan2(vel.y, vel.x) * Mathf.Rad2Deg + 90;
+
+        staffPivot.transform.eulerAngles = new Vector3(0, 0, angle);
+        transform.position = pos1;
 
     }
 
