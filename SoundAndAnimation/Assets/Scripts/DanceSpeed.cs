ee36d3ec3ed7f76e768bd6e0041df334fb350ca6
erikcrossdev <erikcruzdev@gmail.com>
2022-10-22 20:25:40 -0300

    New songs

--- SoundAndAnimation/Assets/Scripts/DanceSpeed.cs
+++ SoundAndAnimation/Assets/Scripts/DanceSpeed.cs
@@ -10,12 +10,6 @@ public class DanceSpeed : MonoBehaviour
     public Transform lookAt;
 
     public List<AnimationClip> LookFrontClips = new List<AnimationClip>();
-    // Start is called before the first frame update
-    void Awake()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
